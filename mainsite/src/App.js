2e90913596e7076bdb0b2c50583d589bfed56e09
anthonyvigz <anthonyvigz@gmail.com>
2019-12-11 21:25:33 -0500

    set up the pages as component views

--- mainsite/src/App.js
+++ mainsite/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.css';
+import Home from './views/Home';
+import About from './views/About';
+import Capabilities from './views/Capabilities';
+import Industries from './views/Industries';
+import Contact from './views/Contact';
 
 function App() {
   return (
@@ -9,41 +14,17 @@ function App() {
       <ul className="nav">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#services">Services</a></li>
+        <li><a href="#capabilities">Capabilities</a></li>
+        <li><a href="#industries">Industries</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
         </div>
-        <div className="banner-area" id="home">
-            <div className="text-part">
-              <h1>Banner Area</h1>
-              <p>blah</p>
-              </div>
-        </div>
-          <div className="about-area" id="about">
-            <div className="text-part">
-              <h1>About Area</h1>
-              <p>blah</p>
-            </div>
-          </div>
-          <div className="port-area" id="portfolio">
-            <div className="text-part">
-              <h1>Port Area</h1>
-              <p>blah</p>
-            </div>
-          </div>
-          <div className="service-area" id="services">
-            <div className="text-part">
-              <h1>Services Area</h1>
-              <p>blah</p>
-            </div>
-          </div>
-          <div className="contact-area" id="contact">
-            <div className="text-part">
-              <h1>Contact Area</h1>
-              <p>blah</p>
-            </div>
+        <Home />
+        <About />
+        <Capabilities />
+        <Industries />
+        <Contact />
     </div>
-  </div>
   );
 }
 
---
+++ mainsite/src/views/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+        <div className="banner-area" id="home">
+            <div className="text-part">
+                <h1>Banner Area</h1>
+                <p>blah</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
