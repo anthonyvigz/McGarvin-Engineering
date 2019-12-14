1954a84309da8addb9454b7b32c2ea134a6e8a87
anthonyvigz <anthonyvigz@gmail.com>
2019-12-14 11:35:54 -0500

    added new logo

--- mainsite/src/App.js
+++ mainsite/src/App.js
@@ -5,12 +5,13 @@ import About from './views/About';
 import Capabilities from './views/Capabilities';
 import Industries from './views/Industries';
 import Contact from './views/Contact';
+import logo from './img/mainlog.png';
 
 function App() {
   return (
     <div>
       <div className="navbar">
-      <a href="#" className="logo">McGarvin</a>
+      <img src={logo} alt="logo" />
       <ul className="nav">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
