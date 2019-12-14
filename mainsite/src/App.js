cd79649289cb7ed156955582282839728510984b
anthonyvigz <anthonyvigz@gmail.com>
2019-12-14 12:29:55 -0500

    set up static navbar

--- mainsite/src/App.js
+++ mainsite/src/App.js
@@ -6,20 +6,23 @@ import Capabilities from './views/Capabilities';
 import Industries from './views/Industries';
 import Contact from './views/Contact';
 import logo from './img/mainlog.png';
+import './css/navbar.css';
 
 function App() {
   return (
-    <div>
-      <div className="navbar">
-      <img src={logo} alt="logo" />
-      <ul className="nav">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#capabilities">Capabilities</a></li>
-        <li><a href="#industries">Industries</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+    <div className="homeBlock">
+      <div className="bannerBlock">
+        <div className="navbar">
+          <img src={logo} alt="logo" />
+          <ul className="nav">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#capabilities">Capabilities</a></li>
+            <li><a href="#industries">Industries</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
         </div>
+      </div>
         <Home />
         <About />
         <Capabilities />
--- mainsite/src/views/Home.jsx
+++ mainsite/src/views/Home.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 
 function Home() {
     return (
-        <div className="banner-area" id="home">
+        <div className="homescreen" id="home">
             <div className="text-part">
-                <h1>Banner Area</h1>
-                <p>blah</p>
             </div>
         </div>
     )
