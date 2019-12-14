63e213bcfdefdace897c85a632b56ff3ac42b684
anthonyvigz <anthonyvigz@gmail.com>
2019-12-14 13:48:04 -0500

    setting up home page

--- mainsite/src/App.js
+++ mainsite/src/App.js
@@ -6,13 +6,19 @@ import Capabilities from './views/Capabilities';
 import Industries from './views/Industries';
 import Contact from './views/Contact';
 import logo from './img/mainlog.png';
-import './css/navbar.css';
+import './css/home.css';
+import { Spring } from 'react-spring/renderprops';
 
 function App() {
   return (
     <div className="homeBlock">
       <div className="bannerBlock">
-        <div className="navbar">
+      <Spring
+            from={{ opacity: 0 }}
+            to={{ opacity: 1 }}
+            delay='600'>
+            {props => <div style={props}>
+            <div className="navbar">
           <img src={logo} alt="logo" />
           <ul className="nav">
             <li><a href="#home">Home</a></li>
@@ -22,7 +28,24 @@ function App() {
             <li><a href="#contact">Contact</a></li>
           </ul>
         </div>
-        <h2>PRECISION<br/> SHEET METAL<br/> FABRICATION</h2>
+            </div>}
+        </Spring>
+        <Spring
+            from={{ opacity: 0 }}
+            to={{ opacity: 1 }}
+            delay='1100'>
+            {props => <div className="bannerText" style={props}>
+              <h2>PRECISION<br/> SHEET METAL<br/> FABRICATION</h2>
+              <button>MORE ABOUT US</button>
+              <p>
+                At McGarvin Engineering we are able to 
+                produce short run as well as long 
+                production jobs and satisfy all of our 
+                customers needs with our large network 
+                of subcontractors.
+              </p>
+            </div>}
+        </Spring>
       </div>
         <Home />
         <About />
