40a313df73b1b000cd3b199777e7601d735b9994
anthonyvigz <anthonyvigz@gmail.com>
2020-05-18 23:22:46 -0400

    setting up industries page

--- mainsite/src/views/Industries.jsx
+++ mainsite/src/views/Industries.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import "../css/industries.scss";
+import indImg from "../img/factory2-about-pic3.jpg";
 
 function Industries() {
-    return (
-        <div className="indi-area" id="industries">
-            <div className="text-part">
-              <h1>Industries Area</h1>
-              <p>blah</p>
-            </div>
-          </div>
-    )
+  return (
+    <div className="indi-area" id="industries">
+      <div className="mainIndi">
+        <img src={indImg} alt="indImg" />
+        <div className="indiText">
+          <h1>Industries Area</h1>
+          <p>blah</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Industries;
